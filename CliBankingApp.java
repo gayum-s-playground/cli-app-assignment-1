2717b53360e77f8934f5fa7beac887d6e077a78f
Gayum Sasiri <gayumsasiri.gs@gmail.com>
2023-08-15 20:49:06 +0530

    initial commit

---
+++ CliBankingApp.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class CliBankingApp{
+    private static final Scanner SCANNER = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+    }
+}
